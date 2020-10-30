6aee89fe336e16a8f78a6536629a45b2eea935b5
Ermeson Silva Novais <ermeson.android99@gmail.com>
2020-10-30 10:09:26 -0300

    Modifying code style

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function timeConversion(s) {
   s = s.split(":");
-  let hour = parseInt(s[0]);
-  let res;
 
-  if ( hour < 12 && s[2].includes("PM") ) hour += 12;
-  if ( hour < 10 ) hour = "0" + hour;
-  if ( hour == 12 && s[2].includes("AM") ) hour = "00";
+  let hour = parseInt(s[0]);
+  let res = "";
+
+  if (hour < 12 && s[2].includes("PM")){
+    hour += 12;
+  } 
+
+  if ( hour < 10 ) {
+    hour = "0" + hour;
+  }
+
+  if ( hour == 12 && s[2].includes("AM") ) {
+    hour = "00";
+  }
 
   res = `${hour}:${s[1]}:${s[2]}`;
   res = res.includes("AM") 
